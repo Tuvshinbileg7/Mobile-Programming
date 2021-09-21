b76d6a0ae7421f033ca0b5d0bb65c924f21e17d8
Tuvshinbileg7 <90661741+Tuvshinbileg7@users.noreply.github.com>
2021-09-22 00:21:18 +0800

    Update Problem4.dart

--- Week1/Problem4.dart
+++ Week1/Problem4.dart
@@ -1,9 +1,16 @@
-void main() {
-  var year = 400;
-
+void leap(var year) {
   if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
     print('Өндөр жил');
   } else {
     print('Өндөр жил биш');
   }
 }
+
+void main() {
+  leap(1997);
+  leap(1996);
+  leap(1900);
+  leap(2000);
+  leap(2021);
+}
+
