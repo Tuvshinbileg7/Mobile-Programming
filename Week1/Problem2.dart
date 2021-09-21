706e98ed5369433d5dcb651f6326f96338f2ab02
Tuvshinbileg7 <90661741+Tuvshinbileg7@users.noreply.github.com>
2021-09-22 00:46:34 +0800

    Update Problem2.dart

--- Week1/Problem2.dart
+++ Week1/Problem2.dart
@@ -1,24 +1,34 @@
-void main() {
-  var x = 34;
+void raindrops(var x) {
   var r1 = x % 3;
   var r2 = x % 5;
   var r3 = x % 7;
+  var prompt = [];
 
   if (r1 == 0) {
-    print('плинг');
+    prompt.add('плинг');
   }
   if (r2 == 0) {
-    print('планг');
+    prompt.add('планг');
   }
   if (r3 == 0) {
-    print('плонг');
+    prompt.add('плонг');
   }
   if (r1 != 0 && r2 != 0 && r3 != 0) {
     for (int i = 2; i <= x; i = i + 1) {
       while (x % i == 0) {
-        print(i);
+        prompt.add(i);
         x = x ~/ i;
       }
     }
   }
+  print(prompt);
+}
+
+void main() {
+  raindrops(28);
+  raindrops(30);
+  raindrops(34);
+  raindrops(44);
+  raindrops(105);
+  raindrops(2956096);
 }
