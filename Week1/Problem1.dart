6b275dda67695482a5a164b260ae7d195ea97113
Tuvshinbileg7 <90661741+Tuvshinbileg7@users.noreply.github.com>
2021-09-22 00:33:19 +0800

    Update Problem1.dart

--- Week1/Problem1.dart
+++ Week1/Problem1.dart
@@ -1,15 +1,24 @@
-void main() {
-  var x = 5;
-  var y = 1;
+void darts(var x, var y) {
   var r = x * x + y * y;
 
   if (r >= 0 && r <= 1) {
-    print('You got 10 points');
+    print('You hit ($x, $y) and got 10 points. Congratulations!');
   } else if (r > 1 && r <= 25) {
-    print('You got 5 points');
+    print('You hit ($x, $y) and got 5 points. Good job.');
   } else if (r > 25 && r <= 100) {
-    print('You got 1 points');
+    print('You hit ($x, $y) and got 1 points. Keep trying.');
   } else {
-    print('You got 0 points');
+    print('You hit ($x, $y) and got 0 points. Bad Luck.');
   }
+}
+
+void main() {
+  darts(0, 0);
+  darts(-0.8, 0.6);
+  darts(3, -2);
+  darts(5, 8);
+  darts(10, 0);
+  darts(8, -8);
+  darts(9, 4.35889894354);
+  darts(9, 4.35889894355);
 }
